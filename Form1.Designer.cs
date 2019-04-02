df867c5b4ecae3349437bed29263009c34ca81aa
Sinan <sinanyuzuguldu41@gmail.com>
2019-04-02 13:22:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace YazLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::YazLab3.Properties.Resources.Video;
+            this.pictureBox1.Location = new System.Drawing.Point(374, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(414, 356);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 290);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Fotoğraf Yakala";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(275, 328);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "4-4-4",
+            "4-2-2",
+            "4-2-0"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 56);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 126);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "W";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "H";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(275, 244);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Dosya Seç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "fileopen.ico");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Seçeceğiniz Dosyanın Formatını Seçin";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Seçeceğiniz Dosyanın Boyutlarını Belirleyin";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 165);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "RW";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "RH";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(73, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(73, 191);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(101, 258);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 32);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "resize";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(106, 311);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(66, 40);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "RStop";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ıma";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazLab3
+{
+    public partial class Form1 : Form
+    {
+        String dosyaUzantisi;
+        String dosyaadi;
+        double k;
+        Byte[] bdizi;
+        int w = 176;
+        int h = 144;
+        int sayac3 = 0;
+        Bitmap[] fdizi;
+        Bitmap[] rfdizi;
+        int rw, rh;
+
+        Bitmap bitmap;
+        int fsayisi = 0;
+        int tsayac = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void resizer()
+        {
+            if (textBox3.Text != "" && textBox4.Text != "")
+            {
+                rw = Int32.Parse(textBox3.Text);
+                rh = Int32.Parse(textBox4.Text);
+            }
+
+            rfdizi = new Bitmap[fsayisi];
+
+            for (int i = 0; i < fsayisi; i++)
+            {
+                rfdizi[i] = new Bitmap(fdizi[i], new Size(rw, rh));
+            }
+           
+        }
+
+        public void video2(int sayac)
+        {
+
+            if (sayac >= fsayisi)
+            {
+                sayac3 = 0;
+                sayac = 0;
+                timer1.Enabled = false;
+            }
+            pictureBox1.Image = rfdizi[sayac];
+
+        }
+        public void video(int sayac)
+        {
+            if (sayac >= fsayisi)
+            {
+                sayac3 = 0;
+                sayac = 0;
+                timer1.Enabled = false;
+            }
+            pictureBox1.Image = fdizi[sayac];
+
+        }
+
+
+
+        public void show()//
+        {
+
+            int sayac1 = 0;
+            int sayac2 = 0;
+
+            //MessageBox.Show(""+fsayisi);
+            while (sayac2 < fsayisi)
+            {
+                bitmap = new Bitmap(w, h);
+                int periyot = (int)(w * h * (k) * sayac2);
+                for (int i = 0; i < h; i++)
+                {
+                    for (int j = 0; j < w; j++)
+                    {
+                        Color renk = Color.FromArgb(bdizi[sayac1 + periyot], bdizi[sayac1 + periyot], bdizi[(sayac1++) + periyot]);
+                        bitmap.SetPixel(j, i, renk);
+                    }
+                }
+                //pictureBox1.Image = bitmap;
+                fdizi[sayac2] = bitmap;
+                sayac2++;
+                sayac1 = 0;
+
+
+            }
+        }
+
+
+        public void fRead()//
+        {
+            w = Int32.Parse(textBox1.Text);
+            h = Int32.Parse(textBox2.Text);
+            rw = w;
+            rh = h;
+            FileStream fs = new FileStream(dosyaUzantisi, FileMode.Open);
+            //MessageBox.Show("" +(int) fs.Length);
+            int a = 0;
+            int index = 0;
+            bdizi = new Byte[fs.Length];
+            fsayisi = bdizi.Length / (int)(k * w * h);
+            fdizi = new Bitmap[fsayisi];
+            while (a > -1)
+            {
+                a = fs.ReadByte();
+                if (a != -1)
+                {
+                    bdizi[index++] = (Byte)a;
+                    //MessageBox.Show(bdizi.Length+ " "+ bdizi[index-1]);
+                }
+            }
+            fs.Close();
+        }
+
+        private void wmp_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tsayac == 0)
+            {
+                resizer();
+                tsayac++;
+            }
+            
+            timer1.Interval = 40;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+            video(sayac3++);
+        }
+
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            video(sayac3++);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fdizi[sayac3].Save("C:\\Users\\Sinan\\Desktop\\yzlb3\\Anlık Yakalanan Fotoğraflar\\a" + sayac3 + ".bmp");
+            if (rfdizi != null)
+            {
+                rfdizi[sayac3].Save("C:\\Users\\Sinan\\Desktop\\yzlb3\\Anlık Yakalanan Fotoğraflar\\ra" + sayac3 + ".bmp");
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            String s = comboBox1.Text;
+            if (s.Equals("4-4-4"))
+            {
+                k = 3;
+            }
+            else if (s.Equals("4-2-2"))
+            {
+                k = 2;
+            }
+            else
+            {
+                k = 1.5;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tsayac = 0;
+            OpenFileDialog dosyaSec = new OpenFileDialog();
+
+            if (dosyaSec.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo dosyabilgisi = new FileInfo(dosyaSec.FileName);
+                dosyaUzantisi = dosyabilgisi.FullName;
+                dosyaadi = dosyabilgisi.Name;
+
+            }
+            fRead();
+            show();
+
+            Directory.CreateDirectory("C:\\Users\\Sinan\\Desktop\\yzlb3\\" + dosyaadi + "Frame Listesi");
+
+            for (int i = 0; i < fsayisi; i++)
+            {
+                fdizi[i].Save("C:\\Users\\Sinan\\Desktop\\yzlb3\\" + dosyaadi + "Frame Listesi\\a" + i + ".bmp");
+            }
+
+            MessageBox.Show("Videonuz Oynatılmaya Hazır!!");
+
+
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            resizer();
+            timer2.Interval = 40;
+            timer2.Enabled = true;
+            timer1.Enabled = false;
+            video2(sayac3++);
+
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            video2(sayac3++);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false;
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
+
+
